e2ae84ea340e4675d28e71f37225d75289e44783
utgupta27 <utgupta27@gmail.com>
2021-07-25 10:39:07 +0530

    initialising Material App

---
+++ lib/homePage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("COVID-19 Statistics"),
+      ),
+      body: Container(
+        color: Colors.limeAccent,
+      ),
+    );
+  }
+}
