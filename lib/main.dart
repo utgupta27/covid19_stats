1b78a8e71288a9844a57c061bc8b872a4428559f
utgupta27 <utgupta27@gmail.com>
2021-07-25 12:12:23 +0530

    firebase core implemented successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:covid19_stats/homePage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:covid19_stats/homepage/homePage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
